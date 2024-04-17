75fde1e7f46221435c0c78f695adc0521bdd7428
Guilherme Veiga <gui.spv@hotmail.com>
2024-04-17 00:00:56 -0300

    Completed the rest of containers and components

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,10 +1,15 @@
-import './cta.css'
+import './cta.css';
 
-function CTA() {
-  return (
-    <div>CTA</div>
-  )
-}
-
+const CTA = () => (
+  <div className="gpt3__cta">
+    <div className="gpt3__cta-content">
+      <p>Request Early Access to Get Started</p>
+      <h3>Register Today & start exploring the endless possibilities.</h3>
+    </div>
+    <div className="gpt3__cta-btn">
+      <button type="button">Get Started</button>
+    </div>
+  </div>
+);
 // eslint-disable-next-line react-refresh/only-export-components
 export default CTA;
